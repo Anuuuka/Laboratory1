9268ec6e94c2c2b1c6f6d9bfbd173cbd4e6b9343
Anuuuka <32230235+Anuuuka@users.noreply.github.com>
2020-04-08 20:03:17 +0300

    Create numbers_helper.dart

---
+++ numbers_helper.dart
@@ -0,0 +1,31 @@
+import 'package:lab1_rpp/en.dart';
+
+
+/// Returns an array of strings from one-nineteen
+getNumsNames(String locale) {
+  return numNames;
+}
+/// Returns an array of strings from one-nineteen
+getTensNames(String locale) {
+  return tensNames;
+}
+/// Returns zero based on locale
+getZero(String locale) {
+  return zero;
+}
+/// Returns hundred based on locale
+getHundred(String locale) {
+  return hundred;
+}
+/// Returns thousand based on locale
+getThousand(String locale) {
+  return thousand;
+}
+/// Returns million based on locale
+getMillion(String locale) {
+  return million;
+}
+/// Returns billion based on locale
+getBillion(String locale) {
+  return billion;
+}
